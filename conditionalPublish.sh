c4d45be6ad5d8ffde02efab404b12ef25c3912f2
Dhyan <d.muralidharan@yottaa.com>
2016-02-28 18:04:01 -0500

    Running test for normal commits too

--- conditionalPublish.sh
+++ conditionalPublish.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    echo '$TRAVIS_PULL_REQUEST is false, skipping tests and publishing'
-    ./gradlew clean bintrayUpload -Pbuild=${TRAVIS_BUILD_NUMBER} -Prelease=1.0
+    echo '$TRAVIS_PULL_REQUEST is false, running tests and publishing'
+    ./gradlew clean test bintrayUpload -Pbuild=${TRAVIS_BUILD_NUMBER} -Prelease=1.0
 else
-    echo '$TRAVIS_PULL_REQUEST is not false ($TRAVIS_PULL_REQUEST), running tests'
+    echo '$TRAVIS_PULL_REQUEST is not false ($TRAVIS_PULL_REQUEST), running just tests and skipping publish'
     ./gradlew clean test
 fi
