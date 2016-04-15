046dfdb4384224ea482458f4392ffcc0d7959e63
Dhyan <d.muralidharan@yottaa.com>
2016-04-15 11:30:31 -0400

    Changes to fix local run

--- src/main/java/org/elasticsearch/kafka/indexer/KafkaESIndexerProcess.java
+++ src/main/java/org/elasticsearch/kafka/indexer/KafkaESIndexerProcess.java
@@ -12,7 +12,7 @@ public class KafkaESIndexerProcess {
     private static final Logger logger = LoggerFactory.getLogger(KafkaESIndexerProcess.class);
     public static void main(String[] args) throws Exception {
         logger.info("Starting KafkaESIndexerProcess  ");
-        ClassPathXmlApplicationContext indexerContext = new ClassPathXmlApplicationContext("spring/kafka-es-context.xml");
+        ClassPathXmlApplicationContext indexerContext = new ClassPathXmlApplicationContext("spring/kafka-es-context-public.xml");
         indexerContext.registerShutdownHook();
         indexerContext.getBean(JobManagerService.class).processAllThreads();
         logger.info("KafkaESIndexerProcess is started OK");
